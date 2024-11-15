6bb0b08de90c7a66ac5ad16669a255ebf8967249
9kaede12 <169217935 +9kaede12@users.noreply.github.com>
2024-11-15 10:22:05 +0900

    column09完成

---
+++ info_B/L09/column09.py
@@ -0,0 +1,11 @@
+import winsound
+print("サウンド開始")
+winsound.Beep(261, 1000) # ドの周波数
+winsound.Beep(293, 1000) # レの周波数
+winsound.Beep(329, 1000) # ミの周波数
+winsound.Beep(349, 1000) # ファの周波数
+winsound.Beep(392, 1000) # ソの周波数
+winsound.Beep(440, 1000) # ラの周波数
+winsound.Beep(493, 1000) # シの周波数
+winsound.Beep(523, 1000) # ドの周波数
+print("サウンド終わり")
